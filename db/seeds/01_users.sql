a71085931365e6c898f42ecac5cc872a6dffaf91
krismina la <krismina.la@gmail.com>
2022-04-09 02:06:47 +0000

    fix FK reference, fix typos, rename file orders of schema and seeds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,8 +1,8 @@
 INSERT INTO users (
-name, email, password)
+name, email, password, mobile, admin)
 VALUES (
 'Restaurant Owner', 'allisonjackson@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(382)738-4322', true),
 (
-'Customer Sally', 'tristanjacobs@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(382)738-4322'),
+'Customer Sally', 'tristanjacobs@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(382)738-4322', false),
 (
-'Lloyd Jefferson', 'asherpoole@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(382)738-4322');
+'Lloyd Jefferson', 'asherpoole@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(382)738-4322', false);
--- db/seeds/03_order_details.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO order_detail (order_id, item_id, quantity)
-VALUES (1, 1, 1),
-(1, 2, 2),
-(1, 3, 3),
-(1, 4, 10);
