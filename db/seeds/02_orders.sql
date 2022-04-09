fed5c50bfb093f338f76184b4ce3ae71c71ad049
kai-commits <kaimeikle123@gmail.com>
2022-04-08 18:11:29 -0700

    create seeds for orders table

---
+++ db/seeds/02_orders.sql
@@ -0,0 +1,2 @@
+INSERT INTO orders (user_id, status_id, created_at, completed_at, active, price, estimated_time)
+VALUES (2, 1, '2018-02-12T08:00:00.000Z', '2018-02-12T08:11:20.000Z', true, 2500, '15 minutes');
