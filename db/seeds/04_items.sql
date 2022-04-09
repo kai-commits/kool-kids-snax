be8f55557dcdf315a71d1d010ddb8017ee022192
krismina la <krismina.la@gmail.com>
2022-04-09 01:39:48 +0000

    populate items table

--- db/seeds/04_items.sql
+++ db/seeds/04_items.sql
@@ -1,15 +1,5 @@
 INSERT INTO items (name, description, price, url_thumb_photo)
-VALUES ('Tonkotsu', 'Pork belly, bean sprouts, cabbage, green onion, 1/2 egg, nori & sweet corn', 12000, 'https://i2.wp.com/photos.smugmug.com/Easy-Tonkotsu/i-8fbnGkf/0/996a0ce1/X3/Easy%20Tonkotsu%2021-X3.jpg'),
-('Deep Fried Tofu', 'Lightly fried tofu with an airy crunch – covered in savory Tsuyu broth and topped with katsuobushi!', 9000, 'https://pickledplum.com/wp-content/uploads/2018/01/agedashi-tofu-520.jpg');
-
-
-
-
-
-
-
-
-
-
-
-('')
+VALUES ('Tonkotsu', 'Pork belly, bean sprouts, cabbage, green onion, 1/2 egg, nori & sweet corn.', 1200, 'https://i2.wp.com/photos.smugmug.com/Easy-Tonkotsu/i-8fbnGkf/0/996a0ce1/X3/Easy%20Tonkotsu%2021-X3.jpg'),
+('Agedashi Tofu', 'Lightly fried tofu with an airy crunch – covered in savory Tsuyu broth and topped with katsuobushi!', 600, 'https://pickledplum.com/wp-content/uploads/2018/01/agedashi-tofu-520.jpg'),
+('Okonomiyaki', 'Savory Japanese pancake made with flour, eggs, shredded cabbage, pork and topped with a variety of condiments.', 800, 'https://jobsinjapan.com/wpdata/wp-content/uploads/2021/04/okonomiyaki.jpg'),
+('Matcha Mochi', 'A sweet Japanese rice cake—gets its pretty green color from matcha powder and a creamy sweetness from coconut milk.', 200, 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https://static.onecms.io/wp-content/uploads/sites/9/2015/02/20/201502-r-xl-matcha-mochi.jpg');
