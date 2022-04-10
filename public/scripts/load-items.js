66cdda427d179b3fba0907bd0e3d89656290a2ee
krismina la <krismina.la@gmail.com>
2022-04-10 01:58:20 +0000

    add item container markup

--- public/scripts/load-items.js
+++ public/scripts/load-items.js
@@ -9,12 +9,31 @@ const loadItems = () => {
 
 const renderItems = (itemsDatabase) => {
   for (const item of itemsDatabase) {
-    $('body').append(createItemElement(item));
+    $('.menu-items').append(createItemElement(item));
   }
 };
 
 const createItemElement = (itemData) => { // Dynamically creates new items from template.
+
+  const item_price = Math.round(itemData.price / 100).toFixed(2);
+
+
   return $(`
-    <h1> hello </h1>
+  <div class="item-container">
+  <img class="item-img" src="${itemData.url_thumb_photo}">
+
+  <div class="item-header">
+    <div class="item-title">
+      <span class="menu-item-add"><i class="fa-solid fa-circle-plus"></i></span>
+      <div class="item-name">${itemData.name}</div>
+    </div>
+    <div class="item-price">
+      $${item_price}
+    </div>
+
+  </div>
+  <div class="item-description">
+    <p>${itemData.description}</p>
+  </div>
   `);
 };
