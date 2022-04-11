ea9b948e74566e22fdaf345afffabd6e2e8c2786
krismina la <krismina.la@gmail.com>
2022-04-11 23:41:45 +0000

    when user clicks on checkout button

---
+++ routes/cart.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router  = express.Router();
+const { chkoutOrder } = require('../public/scripts/twilio.js');
+
+// When the user clicks on the checkout button
+module.exports = (db) => {
+  router.post('/submit', (req, res) => {
+    db.query(`
+    `)
+    .then(data => {
+      const cart_details = data.rows;
+      console.log('shopping cart', req.body);
+
+      chkoutOrder();
+    })
+    .catch(err => {
+      res.status(500).json({ error: err.message });
+    });
+  });
+
+  return router;
+}
