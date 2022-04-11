fc696d422a4827dcfe5ade465657906c439a50e4
krismina la <krismina.la@gmail.com>
2022-04-11 23:41:27 +0000

    moved out of routes and into scripts, refactored code

---
+++ public/scripts/twilio.js
@@ -0,0 +1,27 @@
+/*
+  Performs all twilio message creation functions
+*/
+
+
+require('dotenv').config();
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.AUTH_TOKEN;
+
+const twilio = require('twilio');
+const client = new twilio(accountSid, authToken);
+
+
+// Order placed by customer
+// SMS will be sent to restaurant notifying it that an order has been placed
+const chkoutOrder = () => {
+  client.messages
+  .create({
+    body: 'An order has been placed for Kool-Kids Snax. Please update its status on the admin console.',
+    to: process.env.PHONE_NUMBER,
+    from: process.env.TWILIO_NUMBER,
+  })
+  .then((message) => console.log(message.sid));
+};
+
+
+module.exports = { chkoutOrder };
