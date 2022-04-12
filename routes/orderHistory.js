014ecace20b27d6ae1fd1bb942f7cd196318d659
krismina la <krismina.la@gmail.com>
2022-04-12 20:17:59 +0000

    create route to grab all orders based on user id

--- routes/orderHistory.js
+++ routes/orderHistory.js
@@ -3,17 +3,23 @@ const router  = express.Router();
 
 // Populate order summary for customer
 module.exports = (db) => {
-  router.get('/order_history', (req, res) => {
+  router.get('/', (req, res) => {
+    console.log(req.session)
     db.query(`
       SELECT
-        orders.*, order_details.*, items.*
+        orders.*, statuses.*, estimated_times.*
       FROM orders
-      JOIN order_details ON orders.id = order_details.order_id
-      JOIN items ON order_details.item_id = items.id
-      RETURNING *;
+      JOIN statuses ON orders.status_id = statuses.id
+      JOIN estimated_times ON orders.estimated_time_id = estimated_times.id
+      WHERE orders.user_id = ${req.session.user.id}
+      ;
     `)
       .then((data) => {
-        console.log(data);
+        // console.log('data', data.rows);
+        // console.log('req session', req.session);
+        const order_history = data.rows;
+        res.json({ order_history });
+
       })
       .catch(err => {
         res
@@ -24,5 +30,12 @@ module.exports = (db) => {
 
   return router;
 }
+
+// SELECT
+// orders.*, order_details.*, items.*
+// FROM orders
+// JOIN order_details ON orders.id = order_details.order_id
+// JOIN items ON order_details.item_id = items.id
+// WHERE orders.user_id = ${req.session.user.id}
 
 
