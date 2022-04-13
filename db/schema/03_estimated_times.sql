4ae3a78941932af03a808b355d07ca6fafcf5698
krismina la <krismina.la@gmail.com>
2022-04-13 03:00:28 +0000

    fix data entry

--- db/schema/03_estimated_times.sql
+++ db/schema/03_estimated_times.sql
@@ -4,5 +4,5 @@ DROP TABLE IF EXISTS estimated_times CASCADE;
 
 CREATE TABLE estimated_times (
   id SERIAL PRIMARY KEY NOT NULL,
-  time INTEGER NOT NULL
+  time VARCHAR(50) NOT NULL
 );
--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -2,14 +2,9 @@ INSERT INTO users (
 name, email, password, mobile, admin)
 VALUES (
 'kool-kids-klub', 'allisonjackson@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(382)738-4322', true),
-(
-'Sassy Sally', 'tristanjacobs@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(382)738-4322', false),
-(
-'Lloyd Jefferson', 'asherpoole@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(382)738-4322', false),
-(
-'Pooh Bear', 'asherpoole@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(382)738-4322', false),
-(
-'Stuart Little', 'asherpoole@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(382)738-4322', false),
-(
-'Garfield the Cat', 'asherpoole@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(382)738-4322', false)
+('Sassy Sally', 'tristanjacobs@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(382)738-4322', false),
+('Lloyd Jefferson', 'asherpoole@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(382)738-4322', false),
+('Pooh Bear', 'asherpoole@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(382)738-4322', false),
+('Stuart Little', 'asherpoole@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(382)738-4322', false),
+('Garfield the Cat', 'asherpoole@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(382)738-4322', false)
 ;
