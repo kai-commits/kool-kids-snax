a89272593a5b312f32d37a9214dd5a7dd41c4272
krismina la <krismina.la@gmail.com>
2022-04-13 02:43:41 +0000

    add more test users

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -3,6 +3,13 @@ name, email, password, mobile, admin)
 VALUES (
 'Restaurant Owner', 'allisonjackson@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(382)738-4322', true),
 (
-'Customer Sally', 'tristanjacobs@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(382)738-4322', false),
+'Sassy Sally', 'tristanjacobs@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(382)738-4322', false),
 (
-'Lloyd Jefferson', 'asherpoole@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(382)738-4322', false);
+'Lloyd Jefferson', 'asherpoole@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(382)738-4322', false),
+(
+'Pooh Bear', 'asherpoole@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(382)738-4322', false),
+(
+'Stuart Little', 'asherpoole@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(382)738-4322', false),
+(
+'Garfield the Cat', 'asherpoole@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(382)738-4322', false)
+;
