04af0f47738310db5b24c4a9cadd3aeba212b3cf
krismina la <krismina.la@gmail.com>
2022-04-13 23:28:47 +0000

    new route to add item to items table

--- routes/add-item.js
+++ routes/add-item.js
@@ -3,10 +3,13 @@ const router  = express.Router();
 
 module.exports = (db) => {
   router.post("/add-item", (req, res) => {
+
     db.query(
       `INSERT INTO items (name, description, price, url_thumb_photo, menu_group_id)
       VALUES ($1, $2, $3, $4, $5,)
-      RETURNING *;`, ??????? )
+      RETURNING *;
+      `)
+      .then()
       .catch(err => {
         res
         .status(500)
@@ -14,5 +17,6 @@ module.exports = (db) => {
       });
       res.redirect('/');
   });
+
   return router;
 };
