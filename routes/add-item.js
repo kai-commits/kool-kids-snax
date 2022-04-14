67ec98fc27c18451220ee33463bde804fdabbe10
krismina la <krismina.la@gmail.com>
2022-04-14 01:14:44 +0000

    route to insert item into table

--- routes/add-item.js
+++ routes/add-item.js
@@ -2,21 +2,29 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
-  router.post("/add-item", (req, res) => {
+  router.post("/", (req, res) => {
+    console.log('this', req.body);
 
     db.query(
-      `INSERT INTO items (name, description, price, url_thumb_photo, menu_group_id)
-      VALUES ($1, $2, $3, $4, $5,)
+      `
+      INSERT INTO items (name, description, price, url_thumb_photo, menu_group_id)
+      VALUES ('${req.body.name}', '${req.body.desc}', ${Number(req.body.price)}, '${req.body.img}', ${req.body.group})
       RETURNING *;
-      `)
-      .then()
+      `
+      )
+      .then(() => {
+        console.log('added to menu!');
+        res.redirect('/');
+      })
       .catch(err => {
+        console.log(err);
         res
         .status(500)
         .json({ error: err.message });
       });
-      res.redirect('/');
+      // res.redirect('/');
   });
 
   return router;
 };
+
