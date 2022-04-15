f9033fe5c70d2aef6741bbe965b0a6f7e16a18a1
krismina la <krismina.la@gmail.com>
2022-04-15 00:06:05 +0000

    move admin user to last of list

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,10 +1,10 @@
-INSERT INTO users (
+NSERT INTO users (
 name, email, password, mobile, admin)
-VALUES (
-'admin', 'allisonjackson@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(382)738-4322', true),
-('Sassy Sally', 'tristanjacobs@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(382)738-4322', false),
+VALUES ('Sassy Sally', 'tristanjacobs@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(382)738-4322', false),
 ('Lloyd Jefferson', 'asherpoole@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(382)738-4322', false),
 ('Pooh Bear', 'asherpoole@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(382)738-4322', false),
 ('Stuart Little', 'asherpoole@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(382)738-4322', false),
-('Garfield the Cat', 'asherpoole@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(382)738-4322', false)
+('Garfield the Cat', 'asherpoole@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(382)738-4322', false),
+(
+'admin', 'allisonjackson@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(382)738-4322', true)
 ;
