1edb74321b1cbc0082b09a1d92185a3a4ece57b8
krismina la <krismina.la@gmail.com>
2022-04-15 03:03:37 +0000

    change desc from text to varchar

--- db/schema/06_items.sql
+++ db/schema/06_items.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS items CASCADE;
 CREATE TABLE items (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
-  description TEXT,
+  description VARCHAR(500),
   price INTEGER NOT NULL DEFAULT 0,
   url_thumb_photo VARCHAR(255) NOT NULL,
   menu_group_id INTEGER REFERENCES menu_groups(id) ON DELETE CASCADE
