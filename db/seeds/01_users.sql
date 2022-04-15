4a86b76bb3f4acdffacae18283c652330ca00fc0
krismina la <krismina.la@gmail.com>
2022-04-15 00:10:11 +0000

    fix typo

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,4 +1,4 @@
-NSERT INTO users (
+INSERT INTO users (
 name, email, password, mobile, admin)
 VALUES ('Sassy Sally', 'tristanjacobs@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(382)738-4322', false),
 ('Lloyd Jefferson', 'asherpoole@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '(382)738-4322', false),
